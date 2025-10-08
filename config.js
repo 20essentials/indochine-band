8c0bf1fb1091b5cbd9a5a89830a912efab9080f0
DominicDev <s0l0estudi4r@gmail.com>
2025-10-08 18:17:10 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'INDOCHINE';
+export const nameSongs = [
+	"L’AVENTURIER", /*1*/
+	"SALÔMBO", /*2*/
+	"3E SEXE", /*3*/
+	"TES YEUX NOIRS", /*4*/
+	"ELECTRASTAR", /*5*/
+	"CANARY BAY", /*6*/
+	"TROIS NUITS PAS SEMAINE", /*7*/
+	"COLLEGE BOY", /*8*/
+	"KAO BANG", /*9*/
+	"LE BAISER ", /*10*/
+	"LE DERNIER JOUR", /*11*/
+	"LE GRAND SECRET", /*12*/
+	"LA GUERRE EST FINIE", /*13*/
+	"LES TZARS ", /*14*/
+	"ALICE & JUNE", /*15*/
+	"SONG FOR A DREAM", /*16*/
+	"J'AI DEMANDÉ À LA LUNE", /*17*/
+	"UN ÉTÉ FRANÇAIS", /*18*/
+	"LA CHEVAUCHÉE DES CHAMPS DE BLÉ", /*19*/
+	"LA MACHINE À RATTRAPER LE TEMPS", /*20*/
+]
